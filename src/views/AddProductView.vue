4d2b2b4f5b854cba1ec14b1fa1da5eacf4a88e7f
Ouifi23 <amaury.imhoff@apprenant.ifapme.be>
2025-01-11 16:38:43 +0100

    Change text in french

--- src/views/AddProductView.vue
+++ src/views/AddProductView.vue
@@ -1,38 +1,37 @@
 <script setup>
-  import { ref } from 'vue'
-  import { useProductsStore } from "../stores/product"
+import { ref } from "vue";
+import { useProductsStore } from "../stores/product";
 
-  const store = useProductsStore()
+const store = useProductsStore();
 
-  const newProduct = ref({
-    price: 0,
-    description: '',
-    img: '',
-    favorite: false
-  })
+const newProduct = ref({
+  price: 0,
+  description: "",
+  img: "",
+  favorite: false,
+});
 
-  const addProduct = () => {
-    store.addProduct( newProduct.value)
-  }
-  
+const addProduct = () => {
+  store.addProduct(newProduct.value);
+};
 </script>
 
 <template>
   <main class="home-container">
-    <h1>Add Product</h1>
+    <h1>Ajouter un produit</h1>
 
     <form action="" class="form" @submit.prevent="addProduct">
       <div class="control">
         <label for="">Description</label>
-        <input type="text" v-model.lazy.trim="newProduct.description">
+        <input type="text" v-model.lazy.trim="newProduct.description" />
       </div>
       <div class="control">
         <label for="">Prix</label>
-        <input type="number" v-model.lazy="newProduct.price">
+        <input type="number" v-model.lazy="newProduct.price" />
       </div>
       <div class="control">
         <label for="">Image</label>
-        <input type="text" v-model.lazy.trim="newProduct.img">
+        <input type="text" v-model.lazy.trim="newProduct.img" />
       </div>
       <div class="control">
         <button>Envoyer</button>
@@ -43,7 +42,7 @@
 </template>
 
 <style>
-  label{
-    display: block;
-  }
+label {
+  display: block;
+}
 </style>
